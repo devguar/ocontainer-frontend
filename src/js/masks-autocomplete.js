1e05b30baf426c76e939b9c2419d0978d4eb6857
Lucas Guarnieri <lucasguarni@gmail.com>
2017-01-04 17:38:04 -0200

    Alteração para diminuir carga inicial da js

---
+++ src/js/masks-autocomplete.js
@@ -0,0 +1,98 @@
+/**
+ * Created by lucas.guarnieri on 03/11/2016.
+ */
+
+function defineMasksAutocomplete() {
+    $('.mask-chosen').select2({
+        theme: "bootstrap",
+        placeholder: "Selecione uma opção",
+        language: 'pt-BR',
+    });
+
+    var options_ac = {
+        theme: "bootstrap",
+        placeholder: "Selecione uma opção",
+        language: 'pt-BR',
+        minimumInputLength: 1,
+        minimumResultsForSearch: 10,
+        ajax: {
+            dataType: "json",
+            type: "GET",
+            data: function (params) {
+                return {termo: params.term};
+            },
+            processResults: function (data) {
+                return {
+                    results: $.map(data, function (item) {
+                        return {
+                            text: item.text,
+                            id: item.id
+                        }
+                    })
+                };
+            }
+        }
+    };
+
+    var options_ac_allowclear = Object.create(options_ac);
+    options_ac_allowclear.allowClear = true;
+
+    var options_ac_autoload = Object.create(options_ac);
+    options_ac_autoload.minimumInputLength = 0;
+
+    var options_ac_autoload_allowclear = Object.create(options_ac_autoload);
+    options_ac_autoload_allowclear.allowClear = true;
+
+    var options_ac_allowinsert = Object.create(options_ac);
+    options_ac_allowinsert.tags = true;
+    options_ac_allowinsert.createTag = function (params) {
+        return {
+            id: "new:" + params.term,
+            text: params.term,
+            newOption: true
+        }
+    };
+
+    var options_ac_autoload_allowinsert = Object.create(options_ac_allowinsert);
+    options_ac_autoload_allowinsert.minimumInputLength = 0;
+
+    $(".mask-chosen-autocomplete").select2(options_ac);
+    $('.mask-chosen-autocomplete-allow-deselect').select2(options_ac_allowclear);
+    $(".mask-chosen-autocomplete-autoload").select2(options_ac_autoload);
+    $('.mask-chosen-autocomplete-allow-deselect-autoload').select2(options_ac_autoload_allowclear);
+    $('.mask-chosen-autocomplete-allow-insert').select2(options_ac_allowinsert);
+    $('.mask-chosen-autocomplete-allow-insert-autoload').select2(options_ac_autoload_allowinsert);
+
+    $('.mask-chosen-cidade_id').select2({
+        theme: "bootstrap",
+        placeholder: "Selecione uma opção",
+        language: 'pt-BR',
+        minimumInputLength: 1,
+        minimumResultsForSearch: 10,
+        ajax: {
+            url: function(){
+                var estadoid = $("#estado_id").val();
+
+                var urlcontroller = urlcidades;
+                urlcidades = urlcidades.replace(/999999/g, estadoid);
+                return urlcontroller;
+            },
+            dataType: "json",
+            type: "GET",
+            data: function (params) {
+                return {termo: params.term};
+            },
+            processResults: function (data) {
+                return {
+                    results: $.map(data, function (item) {
+                        return {
+                            text: item.text,
+                            id: item.id
+                        }
+                    })
+                };
+            }
+        },
+        allowClear: true
+    });
+}
--- src/js/masks.js
+++ src/js/masks.js
@@ -11,8 +11,7 @@ $(document).ready(function(){
 });
 
 function defineMasks() {
-    $(".mask-data").mask("99/99/9999");
-    $(".mask-data").datepicker({language: 'pt-BR'});
+    $(".mask-data").mask("99/99/9999").datepicker({language: 'pt-BR'});
 
     $(".mask-hora").mask("99:99");
 
@@ -49,85 +48,6 @@ function defineMasks() {
         }
     });
 
-    $('.mask-chosen').select2({
-        theme: "bootstrap",
-        placeholder: "Selecione uma opção",
-        language: 'pt-BR',
-    });
-
-    var options_ac = {
-        theme: "bootstrap",
-        placeholder: "Selecione uma opção",
-        language: 'pt-BR',
-        minimumInputLength: 1,
-        minimumResultsForSearch: 10,
-        ajax: {
-            dataType: "json",
-            type: "GET",
-            data: function (params) {
-                return {termo: params.term};
-            },
-            processResults: function (data) {
-                return {
-                    results: $.map(data, function (item) {
-                        return {
-                            text: item.text,
-                            id: item.id
-                        }
-                    })
-                };
-            }
-        }
-    };
-
-    var options_ac_allowclear = Object.create(options_ac);
-    options_ac_allowclear.allowClear = true;
-
-    var options_ac_autoload = Object.create(options_ac);
-    options_ac_autoload.minimumInputLength = 0;
-
-    var options_ac_autoload_allowclear = Object.create(options_ac_autoload);
-    options_ac_autoload_allowclear.allowClear = true;
-
-    $(".mask-chosen-autocomplete").select2(options_ac);
-    $('.mask-chosen-autocomplete-allow-deselect').select2(options_ac_allowclear);
-    $(".mask-chosen-autocomplete-autoload").select2(options_ac_autoload);
-    $('.mask-chosen-autocomplete-allow-deselect-autoload').select2(options_ac_autoload_allowclear);
-
-    $('.mask-chosen-cidade_id').select2({
-        theme: "bootstrap",
-        placeholder: "Selecione uma opção",
-        language: 'pt-BR',
-        minimumInputLength: 1,
-        minimumResultsForSearch: 10,
-        ajax: {
-            url: function(){
-                var estadoid = $("#estado_id").val();
-
-                var urlcontroller = urlcidades;
-                urlcidades = urlcidades.replace(/999999/g, estadoid);
-                return urlcontroller;
-            },
-            dataType: "json",
-            type: "GET",
-            data: function (params) {
-                return {termo: params.term};
-            },
-            processResults: function (data) {
-                return {
-                    results: $.map(data, function (item) {
-                        return {
-                            text: item.text,
-                            id: item.id
-                        }
-                    })
-                };
-            },
-
-        },
-        allowClear: true
-    });
-
     $('.mask-decimal').maskMoney({
         prefix: '',
         allowNegative: false,
@@ -160,4 +80,6 @@ function defineMasks() {
     });
 
     $(".mask-multiselect").multiSelect();
+
+    defineMasksAutocomplete();
 };
