4897a5ee1d3f044b4f61f07891f22090ef635ecd
LucasGuarnieri <lucasguarni@gmail.com>
2017-06-19 16:44:29 -0300

    Frontend

--- src/Helpers/LayoutHelper.php
+++ src/Helpers/LayoutHelper.php
@@ -27,4 +27,22 @@ class LayoutHelper
     public static function closeCard(){
         return '</div></div>';
     }
+
+    public static function openSmallFormCard($title = null){
+        return '<div class="row"><div class="col-lg-8 col-md-10 col-sm-12">'.self::openCard($title);
+    }
+
+    public static function closeFormCard(){
+        return self::closeCard().'</div></div>';
+    }
+
+    public static function buttonsSubmitAndBack(){
+        $div = '<div class="form-group">';
+        $div .= '<div class="col-sm-offset-2 col-sm-10">';
+        $div .= \Form::submit('Salvar', array('class' => 'btn btn-primary'));
+        $div .= '<a href="'.\URL::previous().'" name="previous" class="btn btn-default">Voltar</a>';
+        $div .= '</div>';
+        $div .= '</div>';
+        return $div;
+    }
 }
